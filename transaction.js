6ea7d8d376147239b3722836a511ea25eb2fbdee
Siddhartha Konduru <kondurus@usf.edu>
2024-11-11 15:54:24 -0500

    Update transaction.js

--- transaction.js
+++ transaction.js
@@ -1,37 +1,30 @@
-// importing data
-import { assets, getAssetbyId } from './asset.js';
+// transaction.js
+import { assets, getAssetById } from './asset.js';
 
-// creating transaction class
-export class Transaction {
-    constructor (assetID, type, quantity) {
-        this.assetID = assetID;
+class Transaction {
+    constructor(assetId, type, quantity) {
+        this.assetId = assetId;
         this.type = type;
         this.quantity = quantity;
-        this.asset = getAssetbyId(assetID);
-
-        // logging error if asset does not exist
-if (!this.asset) {
-    throw new Error (`Asset with ID ${assetID} not found`)
-}};
-
-processTransaction () {
-    // adding quantity if transaction inolves buying
-    if (this.type === 'buy') {
-        this.asset.quantity += this.quantity;
+        this.executeTransaction();
     }
-    // subtracting buying if transaction involves selling 
-    else if (this.type === 'sell') {
-        if (this.asset.quantity < this.quantity) {
-            // logging new error if there is not enough quantity for a proper sale
-            throw new Error (`Insufficient quantity for sale of ${this.asset.name}`);
+
+    executeTransaction() {
+        const asset = getAssetById(this.assetId);
+        if (!asset) throw new Error("Asset not found");
+
+        if (this.type === "buy") {
+            asset.quantity += this.quantity;
+        } else if (this.type === "sell") {
+            if (asset.quantity < this.quantity) {
+                throw new Error(`Insufficient quantity for sale of ${asset.name}`);
+            }
+            asset.quantity -= this.quantity;
+        } else {
+            throw new Error("Transaction type must be 'buy' or 'sell'");
         }
-        this.asset.quantity -= this.quantity
-    } else {
-        // logging new error if type of transaction is not buying or selling,
-        throw new Error (`ERROR: ${this.type} is not a valid type of transaction.`);
     }
-    
 }
 
+export { Transaction };
 
-}
