13d866fd653065d908b4c698828016416bf8904c
Siddhartha Konduru <kondurus@usf.edu>
2024-11-11 15:53:28 -0500

    Update portfolio.js

--- portfolio.js
+++ portfolio.js
@@ -1,26 +1,16 @@
-// Task 2: Create the Portfolio Module
+// portfolio.js
+import { assets } from './asset.js';
 
-// Import named exports
-import { assets } from './asset.js'; 
-
-// Exporting fucntion to calculate total portfolio value
-export function calculatePortfolioValue () {
-    return assets.reduce((total, asset) => {
-        return total + (asset.price * asset.quantity);
-    }, 0)
+function calculatePortfolioValue() {
+    return assets.reduce((total, asset) => total + (asset.price * asset.quantity), 0);
 }
 
-// Exporting a function to calculate percentage allocation of each asset in the portfolio
-export function getPortfolioAllocation () {
-    // calling total value to form percentages
-    let portfolioValue = calculatePortfolioValue ()
-
-    // using map to create new array for percentage allocations
-    return assets.map (asset => ({
-      name: asset.name,
-      allocation: ((asset.price * asset.quantity) / portfolioValue) * 100, 
-    }))
+function getPortfolioAllocation() {
+    const totalValue = calculatePortfolioValue();
+    return assets.map(asset => ({
+        name: asset.name,
+        allocation: ((asset.price * asset.quantity) / totalValue) * 100
+    }));
 }
 
-
-
+export { calculatePortfolioValue, getPortfolioAllocation };
